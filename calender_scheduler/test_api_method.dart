1c7a2163a82b60cf32b6062a11e96fdc12e7a4ff
이준성 <your@email.com>
2025-10-31 21:15:23 +0900

    chore: 나머지 변경사항 추가

--- calender_scheduler/test_api_method.dart
+++ calender_scheduler/test_api_method.dart
@@ -1,7 +1,6 @@
 import 'package:rrule/rrule.dart';
 
 void main() {
-  print('=== RecurrenceRule API 테스트 ===\n');
   
   // 금요일, 토요일 반복
   final rrule = RecurrenceRule(
@@ -13,18 +12,13 @@ void main() {
   );
   
   final rruleString = rrule.toString();
-  print('생성된 RRULE: $rruleString');
-  print('접두사 제거: ${rruleString.replaceFirst("RRULE:", "")}\n');
   
   // 2025-10-25 (토요일) 시작
   final dtstart = DateTime.utc(2025, 10, 25);
-  print('DTSTART: $dtstart (${_getWeekdayName(dtstart.weekday)})\n');
   
   // 인스턴스 생성
   final instances = rrule.getInstances(start: dtstart).take(5).toList();
-  print('생성된 인스턴스:');
   for (var instance in instances) {
-    print('  ${instance.toIso8601String().substring(0, 10)} (${_getWeekdayName(instance.weekday)})');
   }
 }
 
